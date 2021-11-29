438d614ef8440707c14275949fbf4c24872b69c0
Iago Calazans <iago.calazans@gmail.com>
2021-11-29 16:10:46 -0300

    docs(*): :memo: updating example code

--- src/test/index.ts
+++ src/test/index.ts
@@ -7,7 +7,7 @@ const mqemitter = new MQEmitterAMQPLib({
 
 mqemitter.startConnection(
   {
-    url: 'amqps://username:password@hostname/db', // URL of your AMQP instance
+    url: 'amqps://username:password@hosname/db', // URL of your AMQP instance
     queues: ['entry:message', 'process:message', 'close:message'], // The queues to attach to
     method: ApplicationType.BOTH
   }, // Your connection method, must be one of: listener, publisher or both.
@@ -28,34 +28,26 @@ mqemitter.startConnection(
         return done();
       }
     );
+
+    const message = new MessageFactory().generate(
+      'conversation:created', // Topic: Required.
+      { // Content: aditional content can be attached to the Message.
+        user: {
+          name: 'Iago Calazans',
+          age: 29
+        }
+      }
+    );
+
+    cb?.emit(
+      message, 'entry:message', undefined,
+      (err) => {
+        console.log(err); // You can retrieve the errors while trying to attach.
+      }
+    );
   }
 );
 
 mqemitter.events.on( // You can retrieve the errors on MQEmitter events.
   'error', (err) => console.log(err)
-);
-
-setTimeout(
-  () => {
-    try {
-      const message = new MessageFactory().generate(
-        'conversation:created', // Topic: Required.
-        { // Content: aditional content can be attached to the Message.
-          user: {
-            name: 'Iago Calazans',
-            age: 29
-          }
-        }
-      );
-
-      mqemitter.emit(
-        message, 'entry:message', undefined,
-        (err) => {
-          console.log(err); // You can retrieve the errors while trying to attach.
-        }
-      );
-    } catch (err) {
-      console.log(err);
-    }
-  }, 5000
 );
